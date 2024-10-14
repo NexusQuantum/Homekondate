873a878b2ab1bcff818905ef9c9e80327c0a8cc2
Commandlabcreeperminecraft <shuukiuesugi1@gmail.com>
2024-10-14 10:01:49 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -2,8 +2,44 @@
 
 node.textContent = 'value';
 
-function add() {
+function add0() {
   let time = prompt('時間を入力')
-  let element = document.querySelector('h3');
-  element.textContent = `${time}時`;
+  let element = document.getElementById('time-1')
+  element.textContent = `朝飯の時間:${time}時`;
+}
+
+function add1() {
+  let time = prompt('時間を入力')
+  let element = document.getElementById('time-0')
+  element.textContent = `朝飯の時間:${time}時`;
+}
+
+function add2() {
+  let time = prompt('時間を入力')
+  let element = document.getElementById('time--1')
+  element.textContent = `朝飯の時間:${time}時`;
+}
+
+function add3() {
+  let time = prompt('時間を入力')
+  let element = document.getElementById('time-2')
+  element.textContent = `朝飯の時間:${time}時`;
+}
+
+function add4() {
+  let time = prompt('時間を入力')
+  let element = document.getElementById('time-3')
+  element.textContent = `朝飯の時間:${time}時`;
+}
+
+function add5() {
+  let time = prompt('時間を入力')
+  let element = document.getElementById('time-4')
+  element.textContent = `朝飯の時間:${time}時`;
+}
+
+function add6() {
+  let time = prompt('時間を入力')
+  let element = document.getElementById('time-5')
+  element.textContent = `朝飯の時間:${time}時`;
 }
